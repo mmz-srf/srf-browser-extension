287ba37491ce0cfbf9373d63de999b5181439a20
pchr-srf <philipp.christen@srf.ch>
2024-01-08 12:30:54 +0100

    add banner in nora and PAC

--- src/banner.js
+++ src/banner.js
@@ -1,21 +1,47 @@
 const getPhaseForBanner = () => {
-  const url = window.location.href;
-  if(url.indexOf('www-test.') >= 0) {
+  const hostName = window.location.hostname;
+
+  const testHosts = ['www-test.srf.ch', 'www-test.rtr.ch', 'nora.dev.srfdigital.ch'];
+  const stageHosts = ['play-web-staging.herokuapp.com', 'www-stage.srf.ch', 'www-stage.rtr.ch', 'nora.int.srfdigital.ch'];
+  const prodHosts = ['play-web.herokuapp.com', 'www.srf.ch', 'www.rtr.ch', 'nora.srfdigital.ch'];
+  const localHosts = ['dev.srf.ch', 'dev.rtr.ch', 'pascal.srf.ch', 'pascal.rtr.ch'];
+
+  if (testHosts.includes(hostName)) {
     return 'TEST';
-  } else if (url.indexOf('www-stage.') >= 0) {
+  } else if (stageHosts.includes(hostName)) {
     return 'STAGE';
-  } else if (url.indexOf('dev.') >= 0 || url.indexOf('pascal.') >= 0) {
-    return 'DEV';
-  } else {
+  } else if (prodHosts.includes(hostName)) {
     return 'PROD';
+  } else if (localHosts.includes(hostName)) {
+    return 'DEV';
   }
-};
+
+  // special cases for PR apps: regex to the rescue
+  const pacTestRegex = /^play-web-pr-\d+\.herokuapp\.com$/;
+  if (pacTestRegex.test(hostName)) {
+    return 'TEST';
+  }
+  const noraTestRegex = /^nora-pr-\d+\.herokuapp\.com$/;
+  if (noraTestRegex.test(hostName)) {
+    return 'TEST';
+  }
+
+  // Nora's dev env has little useful info in the hostName
+  if (hostName === 'localhost' && window.location.port === "6900" && window.location.pathname.startsWith('/ui/')) {
+    return 'DEV';
+  }
+
+  return '';
+}
 
 const addBanner = () => {
-  const banner = document.createElement("div"); 
-  document.body.appendChild(banner); 
-  banner.innerText = getPhaseForBanner();
-  banner.className = 'srf-tools__banner';
+  const text = getPhaseForBanner();
+  if (text) {
+    const banner = document.createElement("div"); 
+    document.body.appendChild(banner); 
+    banner.innerText = text;
+    banner.className = 'srf-tools__banner';
+  }
 };
 
 const removeBanner = () => {
