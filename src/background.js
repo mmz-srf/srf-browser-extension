de1025ffc12ad1bf34f1596da427b63b356d252f
pchr-srf <philipp.christen@srf.ch>
2024-01-11 09:52:30 +0100

    always show context menu entry

--- src/background.js
+++ src/background.js
@@ -1,24 +1,57 @@
-
-let url = '';
+const openUrl = (url) => {
+  chrome.tabs.create({ 'url': url });
+};
 
 chrome.contextMenus.onClicked.addListener((clickData) => {
-  if (url) {
-    chrome.tabs.create({ 'url': url });
-  }
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    if(chrome.runtime.lastError) {
+      console.log('Not a SRF page, probably?');
+    }
+
+    chrome.tabs.sendMessage(tabs[0].id, {action: "getContentInfo"}, (response) => {
+      if (!response || chrome.runtime.lastError) {
+        // Something went wrong
+        console.log("Error!", chrome.runtime.lastError);
+        return;
+      }
+
+      const { urn, phase, portalUrn, uuid } = response;
+
+      if (!urn) {
+        return;
+      }
+
+      const [,, contentClass, contentId] = urn.split(':');
+      const portalId = portalUrn.split(':').reverse()[0];
+
+      const host =
+        phase === 'DEV' ? 'http://localhost:6900' :
+        phase === 'TEST' ? 'https://nora.dev.srfdigital.ch' :
+        phase === 'STAGE' ? 'https://nora.int.srfdigital.ch' :
+        'https://nora.srfdigital.ch';
+
+
+      if (contentClass === 'article') {
+        // temporary: on test, UUID is used, but not yet on other phases
+        if (phase === 'TEST') {
+          openUrl(`${host}/ui/${portalId}/articles/${uuid}`);
+        } else {
+          openUrl(`${host}/ui/${portalId}/articles/${contentId}`);
+        }
+      } else if (contentClass === 'landingpage') {
+        openUrl(`${host}/ui/${portalId}/pages/${contentId}`);
+      } else {
+        // fallback: open nora prod
+        openUrl(host);
+      }
+
+    });
+  });
 });
 
-chrome.runtime.onMessage.addListener((request) => {
-  if (request.action === 'addContextMenu') {
-    let contextMenuItem = {
-      "id": "srfExtensionAction",
-      "title": request.text,
-      "contexts": ["all"]
-    };
-
-    url = request.url;
-    chrome.contextMenus.create(contextMenuItem);
-  } else if (request.action === 'removeContextMenu') {
-    urn = '';
-    chrome.contextMenus.remove("srfExtensionAction");
-  }
-});
+let contextMenuItem = {
+  "id": "srfExtensionAction",
+  "title": "In Nora bearbeiten",
+  "contexts": ["all"]
+};
+chrome.contextMenus.create(contextMenuItem);
