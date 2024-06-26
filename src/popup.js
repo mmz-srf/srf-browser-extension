5278d556a42179e1cff8ef8cc0e89ad628cab9f6
Elia Ritzmann <derelia2004@gmail.com>
2024-06-26 14:32:17 +0200

    feat: Link to Tweety dev and to thread

--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,18 @@
 import getCommentInfo from "./comments";
 
 const showEnvironmentCheckbox = document.getElementById("showEnvironment");
-const contentIdInput = document.getElementById('contentIdInput');
-const copyContentIdButton = document.querySelector('.js-copy-content-id');
+const contentIdInput = document.getElementById("contentIdInput");
+const copyContentIdButton = document.querySelector(".js-copy-content-id");
 
 // load user setting regarding environment from storage, set checkbox' state
 const setBannerStateFromStorage = () => {
-  chrome.storage.sync.get("shouldShowEnvironment", ({ shouldShowEnvironment }) => {
-    showEnvironmentCheckbox.checked = !!shouldShowEnvironment;
-  });
-}
+  chrome.storage.sync.get(
+    "shouldShowEnvironment",
+    ({ shouldShowEnvironment }) => {
+      showEnvironmentCheckbox.checked = !!shouldShowEnvironment;
+    }
+  );
+};
 
 // When the checkbox is changed, save the setting and let the content script know
 const setupBannerCheckboxListener = () => {
@@ -20,49 +23,54 @@ const setupBannerCheckboxListener = () => {
     chrome.storage.sync.set({ shouldShowEnvironment: shouldShowEnvironment });
 
     // send a message to the current tab
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, {
-        action: 'showEnvironmentBadge'
+        action: "showEnvironmentBadge",
       });
     });
   });
-}
+};
 
 const onContentIdFound = (contentId, phase, portalUrn, businessUnit, uuid) => {
   contentIdInput.value = contentId;
-  
+
   // idea: loop over all links, replace various placeholders with the correct data:
   // $FE_URL    = https://www.srf.ch (depending on phase)
   // $NORA_URL  = https://nora.srfdigital.ch (depending on phase)
   // $ADMIN_URL = https://admin.cms.zrh.production.srf.mpc (depending on phase)
+  // $TWEETY_URL = https://comments.srfdigital.ch (depending on phase)
   // $ID        = contentId
   // $UUID      = uuid
   // $PORTAL    = portal, e.g. "news"
   // $BU        = business unit, i.e. "rtr" or "srf"
 
-  let frontendUrl, noraUrl, adminUrl;
+  let frontendUrl, noraUrl, adminUrl, tweetyUrl;
 
   switch (phase) {
-    case 'DEV':
-      frontendUrl = 'http://www.dev.srf.ch';
-      noraUrl     = 'http://localhost:6900';
-      adminUrl    = 'http://admin.dev.srf.mpc';
+    case "DEV":
+      frontendUrl = "http://www.dev.srf.ch";
+      noraUrl = "http://localhost:6900";
+      adminUrl = "http://admin.dev.srf.mpc";
+      tweetyUrl = "http://localhost:8050";
       break;
-    case 'TEST':
-      frontendUrl = 'https://www-test.srf.ch';
-      noraUrl     = 'https://nora.dev.srfdigital.ch';
-      adminUrl    = 'https://admin.cms.zrh.test.srf.mpc';
+    case "TEST":
+      frontendUrl = "https://www-test.srf.ch";
+      noraUrl = "https://nora.dev.srfdigital.ch";
+      adminUrl = "https://admin.cms.zrh.test.srf.mpc";
+      tweetyUrl = "https://srf-comments-dev.herokuapp.com";
       break;
-    case 'STAGE':
-      frontendUrl = 'https://www-stage.srf.ch';
-      noraUrl     = 'https://nora.int.srfdigital.ch';
-      adminUrl    = 'https://admin.cms.zrh.stage.srf.mpc';
+    case "STAGE":
+      frontendUrl = "https://www-stage.srf.ch";
+      noraUrl = "https://nora.int.srfdigital.ch";
+      adminUrl = "https://admin.cms.zrh.stage.srf.mpc";
+      tweetyUrl = "https://srf-comments-int.herokuapp.com";
       break;
-    case 'PROD':
+    case "PROD":
     default:
-      frontendUrl = 'https://www.srf.ch';
-      noraUrl     = 'https://nora.srfdigital.ch';
-      adminUrl    = 'https://admin.cms.zrh.production.srf.mpc';
+      frontendUrl = "https://www.srf.ch";
+      noraUrl = "https://nora.srfdigital.ch";
+      adminUrl = "https://admin.cms.zrh.production.srf.mpc";
+      tweetyUrl = "https://comments.srfdigital.ch";
       break;
   }
 
@@ -75,7 +83,8 @@ const onContentIdFound = (contentId, phase, portalUrn, businessUnit, uuid) => {
       .replace("$FE_URL", frontendUrl)
       .replace("$NORA_URL", noraUrl)
       .replace("$ADMIN_URL", adminUrl)
-      .replace("$PORTAL", portalUrn.split(':').reverse()[0])
+      .replace("$TWEETY_URL", tweetyUrl)
+      .replace("$PORTAL", portalUrn.split(":").reverse()[0])
       .replace("$BU", businessUnit)
       .replace("$UUID", uuid);
     element.href = href;
@@ -84,67 +93,77 @@ const onContentIdFound = (contentId, phase, portalUrn, businessUnit, uuid) => {
 
 // no content id found - show an error message and hide the input field
 const onContentIdNotFound = () => {
-  document.querySelector(".js-contentid-container").style.display = 'none';
+  document.querySelector(".js-contentid-container").style.display = "none";
 };
 
 const onInfoGatheringFailed = () => {
-  document.querySelector(".js-contentinfo").style.display = 'none';
-}
+  document.querySelector(".js-contentinfo").style.display = "none";
+};
 
 // depending on the content class, different areas in the popup should be hidden/shown
-const onContentClassFound = contentClass => {
+const onContentClassFound = (contentClass) => {
   if (contentClass === "landingpage") {
-    document.body.setAttribute('data-content-class', 'landingpage');
+    document.body.setAttribute("data-content-class", "landingpage");
   } else if (contentClass === "article") {
-    document.body.setAttribute('data-content-class', 'article');
+    document.body.setAttribute("data-content-class", "article");
   }
-}
+};
 
 const onTickerFound = () => {
-  document.querySelector(".js-ticker-link").style.display = '';
-}
+  document.querySelector(".js-ticker-link").style.display = "";
+};
 
 // get some info about the website via content script (content id and content class)
 const getContentInfo = () => {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    if(chrome.runtime.lastError) {
-      console.log('Not a SRF page, probably?');
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    if (chrome.runtime.lastError) {
+      console.log("Not a SRF page, probably?");
     }
 
-    chrome.tabs.sendMessage(tabs[0].id, {action: "getContentInfo"}, (response) => {
-      if (!response || chrome.runtime.lastError) {
-        // Something went wrong
-        console.log("Error!", chrome.runtime.lastError);
-        onInfoGatheringFailed();
-        return;
-      }
-
-      const { urn, phase, portalUrn, hasTicker, businessUnit, uuid, location } = response;
-
-      if (urn) {
-        const [,, contentClass, contentId] = urn.split(':');
-        onContentIdFound(contentId, phase, portalUrn, businessUnit, uuid);
-        onContentClassFound(contentClass);
-
-        getCommentInfo(location, urn);
-
-        if (hasTicker) {
-          onTickerFound();
+    chrome.tabs.sendMessage(
+      tabs[0].id,
+      { action: "getContentInfo" },
+      (response) => {
+        if (!response || chrome.runtime.lastError) {
+          // Something went wrong
+          console.log("Error!", chrome.runtime.lastError);
+          onInfoGatheringFailed();
+          return;
         }
 
-      } else {
-        onContentIdNotFound();
+        const {
+          urn,
+          phase,
+          portalUrn,
+          hasTicker,
+          businessUnit,
+          uuid,
+          location,
+        } = response;
+
+        if (urn) {
+          const [, , contentClass, contentId] = urn.split(":");
+          onContentIdFound(contentId, phase, portalUrn, businessUnit, uuid);
+          onContentClassFound(contentClass);
+
+          getCommentInfo(location, urn);
+
+          if (hasTicker) {
+            onTickerFound();
+          }
+        } else {
+          onContentIdNotFound();
+        }
       }
-    });
+    );
   });
 };
 
 const setupContentIdCopyListener = () => {
-  copyContentIdButton.addEventListener('click', () => {
+  copyContentIdButton.addEventListener("click", () => {
     navigator.clipboard.writeText(contentIdInput.value);
   });
 };
-
 
 // what to do when the extension is opened
 const onLoad = () => {
